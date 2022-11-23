93067172db3d5313c6e9fc776ba109a4ea69db5f
divyanshagarwall <divyanshagrwl.2@gmail.com>
2022-11-24 00:09:18 +0530

    main

--- login.php
+++ login.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['USER_LOGIN']) && $_SESSION['USER_LOGIN'] == 'yes') {
 ?>
 
 <div class="ht__bradcaump__area"
-	style="background: rgba(0, 0, 0, 0) url(images/bg/4.jpg) no-repeat scroll center center / cover ;">
+	style="background: rgba(0, 0, 0, 0) url(images/bg/6.jpg) no-repeat scroll center center / cover ;">
 	<div class="ht__bradcaump__wrap">
 		<div class="container">
 			<div class="row">
